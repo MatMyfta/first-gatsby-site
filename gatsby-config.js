2e73d0e0e32be78eda5a794ab8c095b9230bd9d0
MatMyfta <mateomyftaraj98@gmail.com>
2021-10-20 17:36:33 +0200

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,18 +13,9 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
     },
   ],
 };
